6a5cb510b105fdd4d404b22c4082eccdd68f3456
Samuel Cruz Anzoategui <samycru3106@gmail.com>
2025-10-22 09:55:45 +0200

    Practica_1

---
+++ src/main/java/com/diw/practica1/Libro.java
@@ -0,0 +1,77 @@
+package com.diw.practica1;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Libro")
+public class Libro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String titulo;
+    private String autor;
+    private String editorial;
+    private String isbn;
+    private int año;
+    private float precio;
+
+    public Libro() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getEditorial() {
+        return editorial;
+    }
+
+    public void setEditorial(String editorial) {
+        this.editorial = editorial;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getAño() {
+        return año;
+    }
+
+    public void setAño(int año) {
+        this.año = año;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+}
